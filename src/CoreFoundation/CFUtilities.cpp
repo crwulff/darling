17d12ebfbb002e47074fbe6c9b00ed6be261165b
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-01-01 01:25:49 -0500

    Add _CFExecutableLinkedOnOrAfter stub.

---
+++ src/CoreFoundation/CFUtilities.cpp
@@ -0,0 +1,9 @@
+
+#include <CoreFoundation/CFUtilities.h>
+
+Boolean _CFExecutableLinkedOnOrAfter(CFSystemVersion version)
+{
+	// TODO: For now just say yep, we are newer
+	return true;
+}
+
---
+++ src/CoreFoundation/CFUtilities.h
@@ -0,0 +1,8 @@
+
+#include <CoreFoundation/CFBase.h>
+
+typedef enum {
+} CFSystemVersion;
+
+CF_EXPORT Boolean _CFExecutableLinkedOnOrAfter(CFSystemVersion version);
+
