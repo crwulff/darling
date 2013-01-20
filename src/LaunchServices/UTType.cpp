ee61f5840080d48a4941b0f3103b820591913661
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-01-20 15:17:59 -0500

    Add a bunch of UTType stubs and correct string values.

--- src/LaunchServices/UTType.cpp
+++ src/LaunchServices/UTType.cpp
@@ -1,6 +1,27 @@
 
 #include "UTType.h"
+#include "../util/log.h"
 
+// Helpers
+class LSContext
+{
+	// TODO
+};
+
+Boolean
+_LSContextInit(LSContext *context)
+{
+	// TODO
+	return true;
+}
+
+void
+_LSContextDestroy(LSContext *context)
+{
+	// TODO
+}
+
+// Type strings
 const CFStringRef kUTExportedTypeDeclarationsKey = CFSTR("UTExportedTypeDeclarations");
 const CFStringRef kUTImportedTypeDeclarationsKey = CFSTR("UTImportedTypeDeclarations");
 const CFStringRef kUTTypeIdentifierKey           = CFSTR("UTTypeIdentifier");
@@ -10,7 +31,240 @@ const CFStringRef kUTTypeDescriptionKey          = CFSTR("UTTypeDescription");
 const CFStringRef kUTTypeIconFileKey             = CFSTR("UTTypeIconFile");
 const CFStringRef kUTTypeReferenceURLKey         = CFSTR("UTTypeReferenceURL");
 const CFStringRef kUTTypeVersionKey              = CFSTR("UTTypeVersion");
-const CFStringRef kUTTagClassFilenameExtension   = CFSTR("UTTagClassFilenameExtension");
-const CFStringRef kUTTagClassMIMEType            = CFSTR("UTTagClassMIMEType");
-const CFStringRef kUTTagClassNSPboardType        = CFSTR("UTTagClassNSPboardType");
-const CFStringRef kUTTagClassOSType              = CFSTR("UTTagClassOSType");
+const CFStringRef kUTTagClassFilenameExtension   = CFSTR("public.filename-extension");
+const CFStringRef kUTTagClassMIMEType            = CFSTR("public.mime-type");
+const CFStringRef kUTTagClassNSPboardType        = CFSTR("com.apple.nspboard-type");
+const CFStringRef kUTTagClassOSType              = CFSTR("com.apple.ostype");
+
+CFStringRef
+UTTypeCreatePreferredIdentifierForTag(CFStringRef inTagClass,
+				      CFStringRef inTag,
+				      CFStringRef inConformingToUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFArrayRef
+UTTypeCreateAllIdentifiersForTag(CFStringRef inTagClass,
+				 CFStringRef inTag,
+				 CFStringRef inConformingToUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFStringRef
+UTTypeCopyPreferredTagWithClass(CFStringRef inUTI,
+				CFStringRef inTagClass)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+Boolean
+UTTypeEqual(CFStringRef inUTI1,
+	    CFStringRef inUTI2)
+{
+	return (kCFCompareEqualTo == CFStringCompare(inUTI1, inUTI2, kCFCompareCaseInsensitive));
+}
+
+Boolean
+UTTypeConformsTo(CFStringRef inUTI,
+		 CFStringRef inConformsToUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return false;
+}
+
+CFStringRef
+UTTypeCopyDescription(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return CFStringCreateCopy(nullptr, inUTI);
+}
+
+CFDictionaryRef
+UTTypeCopyDeclaration(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFURLRef
+UTTypeCopyDeclaringBundleURL(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFStringRef
+UTCreateStringForOSType(OSType inOSType)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+OSType
+UTGetOSTypeFromString(CFStringRef inString)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return 'xxxx';
+}
+
+// Undocumented
+const CFStringRef kUTTagClassDeviceModelCode     = CFSTR("com.apple.device-model-code");
+
+CFArrayRef
+UTTypeCopyChildIdentifiers(CFStringRef inUTI)
+{
+	CFArrayRef result = nullptr;
+	LSContext context;
+	if (_LSContextInit(&context))
+	{
+		result = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
+
+		// TODO
+		LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+
+		_LSContextDestroy(&context);
+	}
+	return result;
+}
+
+CFArrayRef
+UTTypeCopyParentIdentifiers(CFStringRef inUTI)
+{
+	CFArrayRef result = nullptr;
+	LSContext context;
+	if (_LSContextInit(&context))
+	{
+		result = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
+
+		// TODO
+		LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+
+		_LSContextDestroy(&context);
+	}
+	return result;
+}
+
+void
+UTTypeShow(CFStringRef inUTI)
+{
+	CFShow(inUTI);
+}
+
+CFArrayRef
+_UTCopyDeclaredTypeIdentifiers(void)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFArrayRef
+_UTTypeCopyAllTagsWithClass(CFStringRef inUTI, CFStringRef inTagClass)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFDictionaryRef
+_UTTypeCopyDescriptionLocalizationDictionary(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFArrayRef
+_UTTypeCopyDynamicIdentifiersForTags(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFStringRef
+_UTTypeCopyIconFileName(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+CFArrayRef
+_UTTypeCopyPedigree(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return nullptr;
+}
+
+Boolean
+_UTTypeIdentifierIsValid(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return true;
+}
+
+Boolean
+_UTTypeIdentifierIsDynamic(CFStringRef inUTI)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return false;
+}
+
+Boolean
+_UTGetActiveTypeForCFStringIdentifier(LSContext *context, CFStringRef inUTI, void* xxx)
+{
+	// TODO
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return false;
+}
+
+EUTTypeStatus
+_UTTypeGetStatusUsingDB(LSContext *context, CFStringRef inUTI)
+{
+	if (!_UTTypeIdentifierIsValid(inUTI)) return eUTTypeInvalid;
+	if (_UTTypeIdentifierIsDynamic(inUTI)) return eUTTypeDynamic;
+	if (_UTGetActiveTypeForCFStringIdentifier(context, inUTI, nullptr)) return eUTTypeActive;
+	return eUTTypeNormal;
+}
+
+EUTTypeStatus
+_UTTypeGetStatus(CFStringRef inUTI)
+{
+	EUTTypeStatus status = eUTTypeNormal;
+	LSContext context;
+	if (_LSContextInit(&context))
+	{
+		status = _UTTypeGetStatusUsingDB(&context, inUTI);
+		_LSContextDestroy(&context);
+	}
+
+	return status;
+}
+
+CFHashCode
+_UTTypeHash(CFStringRef inUTI)
+{
+	// TODO
+	//return XCFStringHashCaseInsensitive(inUTI);
+	LOG << "FIXME " << __FUNCTION__ << " not implemented" << std::endl;
+	return 0;
+}
+
--- src/LaunchServices/UTType.h
+++ src/LaunchServices/UTType.h
@@ -1,5 +1,6 @@
 
 #include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CFURL.h>
 
 CF_EXPORT const CFStringRef kUTExportedTypeDeclarationsKey;
 CF_EXPORT const CFStringRef kUTImportedTypeDeclarationsKey;
@@ -14,3 +15,85 @@ CF_EXPORT const CFStringRef kUTTagClassFilenameExtension;
 CF_EXPORT const CFStringRef kUTTagClassMIMEType;
 CF_EXPORT const CFStringRef kUTTagClassNSPboardType;
 CF_EXPORT const CFStringRef kUTTagClassOSType;
+
+CF_EXPORT CFStringRef
+UTTypeCreatePreferredIdentifierForTag(CFStringRef inTagClass,
+				      CFStringRef inTag,
+				      CFStringRef inConformingToUTI);
+
+CF_EXPORT CFArrayRef
+UTTypeCreateAllIdentifiersForTag(CFStringRef inTagClass,
+				 CFStringRef inTag,
+				 CFStringRef inConformingToUTI);
+
+CF_EXPORT CFStringRef
+UTTypeCopyPreferredTagWithClass(CFStringRef inUTI,
+				CFStringRef inTagClass);
+
+CF_EXPORT Boolean
+UTTypeEqual(CFStringRef inUTI1,
+	    CFStringRef inUTI2);
+
+CF_EXPORT Boolean
+UTTypeConformsTo(CFStringRef inUTI,
+		 CFStringRef inConformsToUTI);
+
+CF_EXPORT CFStringRef
+UTTypeCopyDescription(CFStringRef inUTI);
+
+CF_EXPORT CFDictionaryRef
+UTTypeCopyDeclaration(CFStringRef inUTI);
+
+CF_EXPORT CFURLRef
+UTTypeCopyDeclaringBundleURL(CFStringRef inUTI);
+
+CF_EXPORT CFStringRef
+UTCreateStringForOSType(OSType inOSType);
+
+CF_EXPORT OSType
+UTGetOSTypeFromString(CFStringRef inString);
+
+// Undocumented
+CF_EXPORT const CFStringRef kUTTagClassDeviceModelCode;
+
+CF_EXPORT CFArrayRef
+UTTypeCopyChildIdentifiers(CFStringRef inUTI);
+
+CF_EXPORT CFArrayRef
+UTTypeCopyParentIdentifiers(CFStringRef inUTI);
+
+CF_EXPORT void
+UTTypeShow(CFStringRef inUTI);
+
+CF_EXPORT CFArrayRef
+_UTCopyDeclaredTypeIdentifiers(void);
+
+CF_EXPORT CFArrayRef
+_UTTypeCopyAllTagsWithClass(CFStringRef inUTI, CFStringRef inTagClass);
+
+CF_EXPORT CFDictionaryRef
+_UTTypeCopyDescriptionLocalizationDictionary(CFStringRef inUTI);
+
+CF_EXPORT CFArrayRef
+_UTTypeCopyDynamicIdentifiersForTags(CFStringRef inUTI);
+
+CF_EXPORT CFStringRef
+_UTTypeCopyIconFileName(CFStringRef inUTI);
+
+CF_EXPORT CFArrayRef
+_UTTypeCopyPedigree(CFStringRef inUTI);
+
+typedef enum
+{
+	eUTTypeInvalid = -1,
+	eUTTypeNormal  = 0,
+	eUTTypeActive  = 1,
+	eUTTypeDynamic = 2
+} EUTTypeStatus;
+
+CF_EXPORT EUTTypeStatus
+_UTTypeGetStatus(CFStringRef inUTI);
+
+CF_EXPORT CFHashCode
+_UTTypeHash(CFStringRef inUTI);
+
