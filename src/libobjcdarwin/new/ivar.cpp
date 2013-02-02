beea0a952eab3c0fef7cb52377af17665061665d
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-02-01 23:59:57 -0500

    Correct Ivar offsets to the ones actually assigned by libobjc2.

--- src/libobjcdarwin/new/ivar.cpp
+++ src/libobjcdarwin/new/ivar.cpp
@@ -11,6 +11,13 @@ void ConvertIvarList(Class c, const ivar_list_t* list)
 
 		LOG << "Ivar: name: " << v->name << "; type: " << v->type << "; offset: " << *v->offset << "; size: " << v->size << "; alignment: " << v->alignment << std::endl;
 		class_addIvar(c, v->name, v->size, v->alignment, v->type);
+		Ivar ivar = class_getInstanceVariable(c, v->name);
+		uintptr_t newOffset = ivar_getOffset(ivar);
+		if (*v->offset != newOffset)
+		{
+			LOG << "Ivar moved from " << *v->offset << " to " << newOffset << std::endl;
+			*v->offset = newOffset;
+		}
 	}
 }
 
