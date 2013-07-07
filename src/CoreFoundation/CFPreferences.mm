9d630ff6badcc4806fd8ba7f193d862c02a2676d
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-07-06 23:37:12 -0400

    CFPreferences: Partial implementation using NSUserDefaults.

--- src/CoreFoundation/CFPreferences.mm
+++ src/CoreFoundation/CFPreferences.mm
@@ -1,6 +1,10 @@
 #include "CFPreferences.h"
 #include <stdio.h>
 
+#include <Foundation/NSUserDefaults.h>
+#include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CFNumber.h>
+
 const CFStringRef kCFPreferencesAnyApplication     = CFSTR("CFPreferencesAnyApplication");
 const CFStringRef kCFPreferencesCurrentApplication = CFSTR("CFPreferencesCurrentApplication");
 const CFStringRef kCFPreferencesAnyHost            = CFSTR("CFPreferencesAnyHost");
@@ -18,26 +22,98 @@ const CFStringRef kCFPreferencesDidChangeObject                  = CFSTR("CFPref
 
 CFPropertyListRef CFPreferencesCopyAppValue(CFStringRef key, CFStringRef applicationID)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	return NULL; // TODO
+	// TODO: this ignores the applicationID and searches more than just the application domain
+
+	return [[NSUserDefaults standardUserDefaults] objectForKey: key];
 }
 
 Boolean CFPreferencesGetAppBooleanValue(CFStringRef key, CFStringRef applicationID, Boolean *keyExistsAndHasValidFormat)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	return false; // TODO
+	Boolean result = false;
+	Boolean valid = false;
+
+	CFPropertyListRef value = CFPreferencesCopyAppValue(key, applicationID);
+
+	if (value)
+	{
+		CFTypeID type = CFGetTypeID(value);
+		if (type == CFStringGetTypeID())
+		{
+			if ((kCFCompareEqualTo == CFStringCompare((CFStringRef)value, CFSTR("true"), kCFCompareCaseInsensitive)) ||
+			    (kCFCompareEqualTo == CFStringCompare((CFStringRef)value, CFSTR("YES"), kCFCompareCaseInsensitive)))
+			{
+				valid = true;
+				result = true;
+			}
+			else if ((kCFCompareEqualTo == CFStringCompare((CFStringRef)value, CFSTR("false"), kCFCompareCaseInsensitive)) ||
+			         (kCFCompareEqualTo == CFStringCompare((CFStringRef)value, CFSTR("NO"), kCFCompareCaseInsensitive)))
+			{
+				valid = true;
+				result = true;
+			}
+		}
+		else if (type == CFNumberGetTypeID())
+		{
+			int number = 0;
+			CFNumberGetValue((CFNumberRef)value, kCFNumberIntType, &number);
+			valid = true;
+			result = (number != 0);
+		}
+		else if (type == CFBooleanGetTypeID())
+		{
+			valid = true;
+			result = (value == kCFBooleanTrue);
+		}
+
+		CFRelease(value);
+	}
+
+	if (keyExistsAndHasValidFormat)
+	{
+		*keyExistsAndHasValidFormat = valid;
+	}
+
+	return result;
 }
 
 CFIndex CFPreferencesGetAppIntegerValue(CFStringRef key, CFStringRef applicationID, Boolean *keyExistsAndHasValidFormat)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	return 0; // TODO
+	CFIndex result = 0;
+	Boolean valid = false;
+
+	CFPropertyListRef value = CFPreferencesCopyAppValue(key, applicationID);
+
+	if (value)
+	{
+		CFTypeID type = CFGetTypeID(value);
+		if (type == CFStringGetTypeID())
+		{
+			// TODO: This doesn't detect invalid strings or deal with larger numbers...
+			result = CFStringGetIntValue((CFStringRef)value);
+			valid = true;
+		}
+		else if (type == CFNumberGetTypeID())
+		{
+			CFNumberGetValue((CFNumberRef)value, kCFNumberCFIndexType, &result);
+			valid = true;
+		}
+
+		CFRelease(value);
+	}
+
+	if (keyExistsAndHasValidFormat)
+	{
+		*keyExistsAndHasValidFormat = valid;
+	}
+
+	return result;
 }
 
 void CFPreferencesSetAppValue(CFStringRef key, CFPropertyListRef value, CFStringRef applicationID)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	//TODO
+	// TODO: this ignores the applicationID
+
+	return [[NSUserDefaults standardUserDefaults] setObject: (id)value forKey: key];
 }
 
 void CFPreferencesAddSuitePreferencesToApp(CFStringRef applicationID, CFStringRef suiteID)
@@ -60,8 +136,9 @@ Boolean CFPreferencesAppSynchronize(CFStringRef applicationID)
 
 CFPropertyListRef CFPreferencesCopyValue(CFStringRef key, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	return NULL; // TODO
+	// TODO: this ignores the applicationID, userName and hostName and searches more than just the application domain
+
+	return [[NSUserDefaults standardUserDefaults] objectForKey: key];
 }
 
 CFDictionaryRef CFPreferencesCopyMultiple(CFArrayRef keysToFetch, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName)
@@ -72,8 +149,9 @@ CFDictionaryRef CFPreferencesCopyMultiple(CFArrayRef keysToFetch, CFStringRef ap
 
 void CFPreferencesSetValue(CFStringRef key, CFPropertyListRef value, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName)
 {
-	printf("%s - Not implemented\n", __FUNCTION__);
-	//TODO
+	// TODO: this ignores the applicationID, userName and hostName
+
+	[[NSUserDefaults standardUserDefaults] setObject: (id)value forKey: key];
 }
 
 void CFPreferencesSetMultiple(CFDictionaryRef keysToSet, CFArrayRef keysToRemove, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName)
